87e32451a0f6b69c227fc8159ed6da817611b891
estt2284 <estt2284@ipt.pt>
2013-05-07 23:47:14 +0100

    nano incorporado

--- portugol_server/main.js
+++ portugol_server/main.js
@@ -1,9 +1,14 @@
 var express = require('express'),
-  http = require('http');
+  http = require('http'),
+  nano = require('nano'),
+  util = require('util');
 
 var app = express();
 app.use(express.bodyParser());
 app.use(express.static(__dirname + '/public'));
+
+var nano_server = nano('http://130.185.82.181:5984');
+var nano_db = nano_server.use('mydb');
 
 
 //POST a /couchset
@@ -11,26 +16,35 @@ app.post('/couchset', function (req, res) {
   console.log("####################################################");
   console.log("ENVIA PARA O COUCHDB");
   console.log("####################################################");
+
+  res.writeHead(200, {'Content-Type': 'application/json'});
   console.log(req.body);
 
-  var headers = {'Content-Type': 'application/json'};
-  res.writeHead(200, headers);
-  //enviar o resultado do get ao couchdb
-  var aux  = {'prop1': 'xpto1', 'prop2': 'xpto2'};
-  res.write(JSON.stringify(aux));
-  res.end();
+  nano_db.insert(req.body, req.body._id, function(err){
+    if (err) {
+      res.write(JSON.stringify({'result': 'nok', 'message': err.message}));
+    } else {
+      res.write(JSON.stringify({'result': 'ok'}));
+    }
+    res.end();
+  });
+  
 });
 
 
 //GET a /couchget
-app.get('/couchget', function (req, res) {
+app.get('/couchget/:document_id', function (req, res) {
   console.log("####################################################");
   console.log("VAI BUSCAR AO COUCHDB");
   console.log("####################################################");
-  console.log(req.query);
+  //console.log(req.query);
 
-  //exemplo sem content-type
-  res.send("bla bla bla");
+  res.writeHead(200, {'content-type':'text/html'});
+
+  nano_db.get(req.params.document_id, function(err, val) {
+    res.write(JSON.stringify(val));
+    res.end();
+  });
 });
 
 app.listen(80, function () {
